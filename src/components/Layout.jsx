b3768be12199a8b51632844e2fc84ee224515dc1
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-05-25 02:00:52 -0700

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
-import Home from '../Pages/Home';
 
 const Layout = () => {
   return (
     <>
       <Header />
       <main className='mb-30'> {/* Space for fixed header */}
-        <Home /> {/* This will render the child route's content */}
+        <Outlet /> {/* This will render the child route's content */}
       </main>
       <Footer />
     </>
